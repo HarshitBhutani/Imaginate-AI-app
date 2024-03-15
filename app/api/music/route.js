59836d32a7c516574cd19b530bb339cbea817920
Harshit Bhutani <harshitbhutani22@gmail.com>
2024-03-15 13:15:49 +0530

    Replicate AI muisc API configured

--- app/api/music/route.js
+++ app/api/music/route.js
@@ -1,40 +1,40 @@
-import OpenAI from 'openai';
+import Replicate from "replicate";
 import {auth} from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
-});
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN  || ""
+})
 
 export async function POST(req){
     try{
         const {userId} = auth();
         const body = await req.json();
-        const {messages} = body;
+        const {prompt} = body;
 
         if(!userId){
             return new NextResponse("Unauthorized", {status: 401});
         }
-        // if(!configuration.apiKey){
-        //     return new NextResponse("OpenAI API Key not configured", {status: 500});
-        // }
-
-        if(!messages){
-            return new NextResponse("Messasges are required", {status:400});
+        
+        if(!prompt){
+            return new NextResponse("Prompt are required", {status:400});
         }
 
+        const response = await replicate.run(
+            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+            {
+              input: {
+                prompt_a: prompt,
+              }
+            }
+          );
+          console.log(response);
         
-        const chatCompletion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages
-          });
-
-          console.log(chatCompletion.choices[0].message);
           return NextResponse.json(chatCompletion.choices[0].message.content);
 
     } catch(error){
-        console.log("[CONVERSATION_ERROR]", error);
+        console.log("[MUISC_ERROR]", error);
         return new NextResponse("Internal error", {status: 500});
     }
 }
