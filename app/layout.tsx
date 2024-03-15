282b131bd5fe6846c26f304011e9dc7dbaddc664
Harshit Bhutani <harshitbhutani22@gmail.com>
2024-03-15 13:44:30 +0530

    Revert "js file change(minor-renaming)"
    This reverts commit 7252ac622a4c036e6b98947b41b0208047ad90c2.
    
    git commit -m "rolling back to prev commit"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
@@ -5,12 +6,16 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata= {
+export const metadata: Metadata = {
   title: "Imaginate",
   description: "AI Platform",
 };
 
-export default function RootLayout({children}) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <ClerkProvider>
 
