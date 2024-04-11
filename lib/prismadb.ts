6b696b429813f56bfbd3519decd8ff621a249d9d
Harshit Bhutani <harshitbhutani22@gmail.com>
2024-04-12 03:04:04 +0530

    fixing

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,22 +1,22 @@
-// import {PrismaClient} from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
 
-// declare global{
-//     var prisma: PrismaClient | undefined
-// }
-
-// const prismadb = global.prisma || new PrismaClient();
-// if(process.env.NODE_ENV !== 'production') global.prisma = prismadb;
-
-// export default prismadb;
-
-
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-	var prismadb: PrismaClient | undefined;
+declare global{
+    var prisma: PrismaClient | undefined
 }
 
-const prismadb = global.prismadb || new PrismaClient({ log: ["info"] });
-if (process.env.NODE_ENV !== "production") global.prismadb = prismadb;
+const prismadb = global.prisma || new PrismaClient();
+if(process.env.NODE_ENV !== 'production') global.prisma = prismadb;
 
 export default prismadb;
+
+
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+// 	var prismadb: PrismaClient | undefined;
+// }
+
+// const prismadb = global.prismadb || new PrismaClient({ log: ["info"] });
+// if (process.env.NODE_ENV !== "production") global.prismadb = prismadb;
+
+// export default prismadb;
