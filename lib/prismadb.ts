84960ffe02d421aae080df3f443883cbc33f4fcc
Harshit Bhutani <harshitbhutani22@gmail.com>
2024-04-12 02:49:08 +0530

    Fixing prisma in deployment

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,26 +1,22 @@
-import {PrismaClient} from "@prisma/client";
+// import {PrismaClient} from "@prisma/client";
 
-declare global{
-    var prisma: PrismaClient | undefined
-}
-
-const prismadb = global.prisma || new PrismaClient();
-if(process.env.NODE_ENV !== 'production') global.prisma = prismadb;
-
-export default prismadb;
-
-
-// import { PrismaClient } from '@prisma/client';
-
-// let prisma: PrismaClient;      // <-- you're missing this!
-
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
+// declare global{
+//     var prisma: PrismaClient | undefined
 // }
 
-// export default prisma;
+// const prismadb = global.prisma || new PrismaClient();
+// if(process.env.NODE_ENV !== 'production') global.prisma = prismadb;
+
+// export default prismadb;
+
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+	var prismadb: PrismaClient | undefined;
+}
+
+const prismadb = global.prismadb || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prismadb = prismadb;
+
+export default prismadb;
