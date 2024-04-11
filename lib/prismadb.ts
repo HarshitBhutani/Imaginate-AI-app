6e5c951be82164fb9c8d53403188546f334d08a3
Harshit Bhutani <harshitbhutani22@gmail.com>
2024-04-12 02:00:03 +0530

    fixing prisma error in deployment

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,7 +4,23 @@ declare global{
     var prisma: PrismaClient | undefined
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
+const prismadb = global.prisma || new PrismaClient();
 if(process.env.NODE_ENV !== 'production') global.prisma = prismadb;
 
 export default prismadb;
+
+
+// import { PrismaClient } from '@prisma/client';
+
+// let prisma: PrismaClient;      // <-- you're missing this!
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
